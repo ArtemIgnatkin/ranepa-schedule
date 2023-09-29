7a2815bd1cc6f3e7a87c6f7f5cad92c7e672880a
ArtemIgnatkin <artemplay2007gg@gmail.com>
2023-09-29 17:25:31 +0700

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeContent(content) {
+    document.getElementById("content").innerHTML = content;
+  }
+  
+  function goToMain() {
+    document.getElementById("content").innerHTML = "Контент главной страницы";
+  }
